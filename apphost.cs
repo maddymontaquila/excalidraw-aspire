ca91f7481d6a3b29f99527bb66d9b9281af894b5
Maddy Montaquila <maleger@microsoft.com>
2025-10-03 19:32:37 -0400

    making aspire work

--- apphost.cs
+++ apphost.cs
@@ -1,48 +1,81 @@
-﻿#:package CommunityToolkit.Aspire.Hosting.NodeJS.Extensions@9.8.0
-#:package Aspire.Hosting.DevTunnels@13.0.0-preview.1.25503.1
-#:sdk Aspire.AppHost.Sdk@13.0.0-preview.1.25503.1
+#:package CommunityToolkit.Aspire.Hosting.NodeJS.Extensions@9.8.0
+#:package Aspire.Hosting.DevTunnels@9.5.1-*
+#:package Aspire.Hosting.Yarp@9.5.1-*
+#:package Aspire.Hosting.Azure.AppContainers@9.5.1
+#:sdk Aspire.AppHost.Sdk@9.5.1
 
 using Aspire.Hosting.DevTunnels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var aca = builder.AddAzureContainerAppEnvironment("aca");
 
 var collabServer = builder.AddYarnApp("collab-server", "./excalidraw-room", "start:dev")
-    .WithHttpEndpoint(env: "PORT");
+    .WithYarnPackageInstallation()
+    .WithHttpEndpoint(env: "PORT")
+    .PublishAsDockerFile(c =>
+    {
+        c.WithEndpoint("http", c => c.UriScheme = "https");
+    })
+    .PublishAsAzureContainerApp((infra, app) =>
+    {
+        app.Template.Scale.MaxReplicas = 1;
+    });
 
-var app = builder.AddYarnApp("excalidraw", "./excalidraw")
-    .WithEnvironment("BROWSER", "none")
-    .WithHttpEndpoint(env: "VITE_APP_PORT")
-    .WithIconName("DrawShape");
-//.WithEnvironment("VITE_APP_WS_SERVER_URL", collabServer.GetEndpoint("http"));
-
-var devTunnel = builder.AddDevTunnel("excalidraw-tunnel-2")
-    .WithAnonymousAccess()
-    .WithReference(collabServer)
-    .WithReference(app);
-
-var tcs = new TaskCompletionSource();
-
-builder.Eventing.Subscribe<ResourceReadyEvent>(devTunnel.Resource, async (e, ct) =>
+if (builder.ExecutionContext.IsRunMode)
 {
-    tcs.SetResult();
-});
+    var app = builder.AddYarnApp("excalidraw-dev", "./excalidraw")
+        .WithYarnPackageInstallation()
+        .WithEnvironment("BROWSER", "none")
+        .WithHttpEndpoint(env: "VITE_APP_PORT")
+        .WithIconName("DrawShape");
 
-app.WithEnvironment(async c =>
-{
-    await tcs.Task;
-});
+    var devTunnel = builder.AddDevTunnel("excalidraw-tunnel")
+        .WithAnonymousAccess()
+        .WithReference(collabServer)
+        .WithReference(app);
 
-#region excalidraw-dockerfile
-builder.AddDockerfile("excalidraw-docker", "./excalidraw")
-    .WithHttpEndpoint(port: 3000, targetPort: 80)
-    .WithEnvironment("NODE_ENV", "development")
-    .WithBuildArg("NODE_ENV", "development")
-    .WithBindMount("./excalidraw", "/opt/node_app/app")
-    .WithBindMount("./excalidraw/package.json", "/opt/node_app/package.json")
-    .WithBindMount("./excalidraw/yarn.lock", "/opt/node_app/yarn.lock")
-    .WithIconName("DrawShape", IconVariant.Regular)
-    .WithExplicitStart();
-#endregion
+    var tcs = new TaskCompletionSource<EndpointReference>(TaskCreationOptions.RunContinuationsAsynchronously);
+    #region Workaround Fowler's lapse of judgement
+    builder.Eventing.Subscribe<ResourceEndpointsAllocatedEvent>(async (e, ct) =>
+    {
+        var logger = e.Services.GetRequiredService<ResourceLoggerService>().GetLogger(e.Resource);
+        logger.LogDebug("Resource endpoints allocated for {resourceName}", e.Resource.Name);
+
+        if (e.Resource is DevTunnelPortResource portResource)
+        {
+            // excalidraw-tunnel-2-excalidraw-http
+            var portResourceName = $"{devTunnel.Resource.Name}-{collabServer.Resource.Name}-http";
+            if (!string.Equals(portResource.Name, portResourceName, StringComparison.OrdinalIgnoreCase))
+            {
+                logger.LogDebug("Skipping tunnel port endpoint for resource {resourceName}", portResource.Name);
+                return;
+            }
+
+            var portEndpoint = portResource.GetEndpoint("tunnel");
+            logger.LogDebug("Tunnel port resource endpoint: {endpoint}", portEndpoint.Url);
+            tcs.SetResult(portEndpoint);
+        }
+    });
+    #endregion
+
+    app.WithEnvironment(async c =>
+    {
+        var tunnelEndpoint = await tcs.Task;
+        c.EnvironmentVariables["VITE_APP_WS_SERVER_URL"] = tunnelEndpoint;
+    });
+}
+
+builder.AddYarp("excalidraw")
+       .WithExternalHttpEndpoints()
+       .WithDockerfile("./excalidraw", "Dockerfile.aspire")
+       .WithStaticFiles()
+       .WithConfiguration(c =>
+       {
+           c.AddRoute("/socket.io/{**catch-all}", collabServer);
+       })
+       .WithExplicitStart();
 
 builder.Build().Run();
